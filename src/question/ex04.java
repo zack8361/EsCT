5aa294ac918ac9772e258e3c9b96fbf16343b976
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-09-01 15:54:29 +0900

    ㄷ
    ㄷ

---
+++ src/question/ex04.java
@@ -0,0 +1,25 @@
+package question;
+
+import java.util.Scanner;
+
+public class ex04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // 구구단 출력할 시작단 입력.
+        int start = sc.nextInt();
+
+        //구구단 끝나는 단 입력
+        int end = sc.nextInt();
+
+
+        for(int i=start; i<=end; i++) {
+            for(int j=1; j<=9; j++) {
+
+            }
+        }
+
+
+    }
+}
