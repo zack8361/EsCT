135df69c303adaf2c35b0e3fba5b51dcc6d67940
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-09-07 17:55:42 +0900

    ex
    ex

---
+++ src/question/ex07.java
@@ -0,0 +1,10 @@
+package question;
+
+import java.util.Scanner;
+
+public class ex07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
