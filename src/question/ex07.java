956c6db9e78c885e08eead2f0c8dc3cb27618f9d
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-09-08 17:50:19 +0900

    ㄷㅌ
    ㄷㅌ

--- src/question/ex07.java
+++ src/question/ex07.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class ex07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int[] arr = new int[4];
+        for(int i =0; i<arr.length; i++) {
+            System.out.println("arr.length = " + arr.length);
+        }
     }
 }
