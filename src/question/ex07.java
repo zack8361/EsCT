b99b9a36a85def5ce44a5e4be61994516273c531
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-09-08 17:57:41 +0900

    승민이 이차원 배열 설명
    승민이 이차원 배열 설명

--- src/question/ex07.java
+++ src/question/ex07.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class ex07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[4];
-        for(int i =0; i<arr.length; i++) {
-            System.out.println("arr.length = " + arr.length);
+        int[][] arr = new int[4][4];
+        for(int i =0; i<arr.length; i++){
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
         }
     }
 }
