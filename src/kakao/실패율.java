57d7fd1f8dbc78e45745d9d70e0fe8d1f8496e1a
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-10-04 16:24:27 +0900

    실패율 풀기
    실패율 풀기

---
+++ src/kakao/실패율.java
@@ -0,0 +1,22 @@
+package kakao;
+
+import java.util.HashMap;
+
+public class 실패율
+{
+    public static void main(String[] args) {
+        int N = 5;
+        int[] stages = {2,1,2,6,2,4,3,3};
+        System.out.println(solution(N,stages));
+    }
+
+    private static int[] solution(int N, int[] stages) {
+        int[] answer=  {};
+        HashMap<Integer,Integer> scoreMap = new HashMap<>();
+//
+//        for (int i = 0; i < stages.length; i++) {
+//            for(int i = )
+//        }
+        return answer;
+    }
+}
