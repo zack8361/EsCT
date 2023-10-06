cbcf18d7ebf4e10ccb9063c58efbea96fd35145d
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-10-06 18:56:42 +0900

    실패율 MAp 쓰면안됨 점수가 같은 경우 생각해야됌ㅁ
    실패율 MAp 쓰면안됨 점수가 같은 경우 생각해야됌ㅁ

--- src/kakao/실패율.java
+++ src/kakao/실패율.java
@@ -1,6 +1,6 @@
 package kakao;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class 실패율
 {
@@ -10,13 +10,41 @@ public class 실패율
         System.out.println(solution(N,stages));
     }
 
-    private static int[] solution(int N, int[] stages) {
-        int[] answer=  {};
-        HashMap<Integer,Integer> scoreMap = new HashMap<>();
-//
-//        for (int i = 0; i < stages.length; i++) {
-//            for(int i = )
-//        }
-        return answer;
+    private static int[] solution(int n, int[] stages) {
+
+        ArrayList<Double> list = new ArrayList<>();
+        HashMap<Double,Integer> scoreMap = new HashMap<>();
+        double ans = 0;
+        double total = stages.length;
+        Arrays.sort(stages);
+        for (double i = 1; i <= n; i++) {
+            double cnt = 0;
+            for (double j = ans; j <stages.length ; j++) {
+                if(i >= stages[(int) j]){
+                    cnt++;
+                    ans = j+1;
+                }
+            }
+            scoreMap.put(cnt/total, (int) i);
+            list.add(cnt/total);
+            total-=cnt;
+        }
+
+        Collections.sort(list,Collections.reverseOrder());
+
+        System.out.println("list = " + list);
+        System.out.println("scoreMap = " + scoreMap);
+        int[] answer = new int[n];
+        int a = 0;
+        for (Double v : list) {
+            if(scoreMap.containsKey(v)){
+                answer[a++] = scoreMap.get(v);
+            }
+        }
+        System.out.println("Arrays.toString(answer) = " + Arrays.toString(answer));
+
+
+
+        return new int[0];
     }
 }
