9f9031ed98429e7b2f0a521adae3c422725644e8
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-10-12 18:58:33 +0900

    키패드 누르기 (생성자 써서 풀어보기)
    키패드 누르기 (생성자 써서 풀어보기)

--- src/kakao/키패드누르기.java
+++ src/kakao/키패드누르기.java
@@ -1,5 +1,7 @@
 package kakao;
 
+import java.util.Arrays;
+
 public class 키패드누르기 {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
@@ -9,8 +11,25 @@ public class 키패드누르기 {
 
     private static String solution(int[] numbers, String hand) {
         String answer = "";
+        int[][] keyPad = {{1,2,3},{4,5,6},{7,8,9},{-1,0,-1}};
 
-        
+        for (int i = 0; i < keyPad.length; i++) {
+            System.out.println(Arrays.toString(keyPad[i]));
+        }
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
+
+            }
+            else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
+
+            }
+            else {
+
+            }
+        }
+
         return answer;
     }
 }
