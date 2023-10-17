df81201a5c668acab69aa4cbdcdc99279e0927ef
zack8361 <71763096+zack8361@users.noreply.github.com>
2023-10-17 18:59:24 +0900

    cT(c)
    cT(c)

---
+++ src/kakao/성격유형검사.java
@@ -0,0 +1,15 @@
+package kakao;
+
+public class 성격유형검사 {
+    public static void main(String[] args) {
+        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+        int[] choices = {5,3,2,7,5};
+        System.out.println(solution(survey,choices));
+    }
+
+    private static String solution(String[] survey, int[] choices) {
+
+        
+        return null;
+    }
+}
--- src/kakao/키패드누르기.java
+++ src/kakao/키패드누르기.java
@@ -1,5 +1,8 @@
 package kakao;
 
+import org.w3c.dom.Node;
+
+import java.util.ArrayDeque;
 import java.util.Arrays;
 
 public class 키패드누르기 {
@@ -8,7 +11,6 @@ public class 키패드누르기 {
         String hand = "right";
         System.out.println(solution(numbers,hand));
     }
-
     private static String solution(int[] numbers, String hand) {
         String answer = "";
         int[][] keyPad = {{1,2,3},{4,5,6},{7,8,9},{-1,0,-1}};
@@ -17,16 +19,44 @@ public class 키패드누르기 {
             System.out.println(Arrays.toString(keyPad[i]));
         }
 
+        int leftHand = 10;
+        int rightHand = 12;
 
         for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
 
+            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
+                leftHand = numbers[i];
+                answer += "L";
             }
             else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
-
+                rightHand = numbers[i];
+                answer += "R";
             }
             else {
+                if(numbers[i] == 0){
+                    numbers[i] = 11;
+                }
+                int leftDist = Math.abs(numbers[i]-leftHand)/3 + Math.abs(numbers[i]-leftHand)%3;
+                int rightDist = Math.abs(numbers[i]-rightHand)/3 + Math.abs(numbers[i]-rightHand)%3;
 
+                if(leftDist<rightDist){
+                    answer += "L";
+                    leftHand = numbers[i];
+                }
+                else if(leftDist>rightDist){
+                    answer +="R";
+                    rightHand = numbers[i];
+                }
+                else {
+                    if(hand.equals("right")){
+                        answer += "R";
+                        rightHand = numbers[i];
+                    }
+                    else {
+                        answer += "L";
+                        leftHand = numbers[i];
+                    }
+                }
             }
         }
 
