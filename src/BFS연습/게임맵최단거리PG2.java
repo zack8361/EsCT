c990f1efa476b42cb71a44f0e47729331e2a74ef
zack8361 <zack6353@naver.com>
2024-01-29 00:13:31 +0900

    esct
    esct

--- src/BFS연습/게임맵최단거리PG2.java
+++ src/BFS연습/게임맵최단거리PG2.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class 게임맵최단거리PG2 {
     private static int[] dx = {1,0,-1,0};
     private static int[] dy = {0,1,0,-1};
+    private static boolean[][] visited;
     public static void main(String[] args) {
         int[][] maps = {
                 {1,0,1,1,1},
@@ -27,10 +28,13 @@ public class 게임맵최단거리PG2 {
     }
 
     private static int solution(int[][] maps) {
-        int answer = 0;
 
+        int[][] mapCopy = new int[maps.length][maps[0].length];
+        visited = new boolean[maps.length][maps[0].length];
+        mapCopy[0][0] = 1;
         ArrayDeque<Node> deque = new ArrayDeque<>();
         deque.addLast(new Node(0,0));
+        visited[0][0] = true;
 
 
         while (!deque.isEmpty()) {
@@ -39,20 +43,31 @@ public class 게임맵최단거리PG2 {
                 int nx = now.x + dx[k];
                 int ny = now.y + dy[k];
 
-                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps.length) {
+                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length) {
                     continue;
                 }
+
                 if (maps[nx][ny] == 0) {
                     continue;
                 }
-                if(maps[nx][ny] == 1) {
-                    deque.addLast(new Node(nx, ny));
-                    maps[nx][ny] = maps[now.x][now.y] + 1;
+                if(!visited[nx][ny] && maps[nx][ny] != 0){
+                    deque.addLast(new Node(nx,ny));
+                    visited[nx][ny] = true;
+                    mapCopy[nx][ny] = mapCopy[now.x][now.y] + 1;
                 }
             }
         }
 
-        return maps[maps.length-1][maps.length-1] == 0 ? - 1 : maps[maps.length-1][maps.length-1];
+        for (int i = 0; i < mapCopy.length; i++) {
+            for (int j = 0; j < mapCopy[i].length; j++) {
+                System.out.print(mapCopy[i][j]);
+            }
+            System.out.println();
+        }
+        if(mapCopy[maps.length-1][maps[0].length-1] == 0){
+            return -1;
+        }
+        return mapCopy[maps.length-1][maps[0].length-1];
     }
 
 }
