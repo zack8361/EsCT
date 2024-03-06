117719a2d42a17c2df823560aecd405895e9e6a3
zack8361 <zack6353@naver.com>
2024-03-07 01:14:08 +0900

    Update 뉴스클러스터링.java
    수정 1

--- src/Lv2/뉴스클러스터링.java
+++ src/Lv2/뉴스클러스터링.java
@@ -4,29 +4,29 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class 뉴스클러스터링 {
+    private static int count;
 
+    private static double cnt;
     private static HashSet<String> set;
+    private static HashMap<String ,Integer> map;
     public static void main(String[] args) {
-        String str1 = "F+RANCE";
+        String str1 = "FRANCE";
         String str2 = "french";
         System.out.println(solution(str1,str2));
     }
 
     private static int solution(String str1, String str2) {
         set = new HashSet<>();
+        map = new HashMap<>();
         int answer = 0;
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
-
-        int current = 0;
-        HashMap<String, Integer> map = new HashMap<>();
-
         addSet(str1);
-
+        count++;
         addSet(str2);
-        System.out.println("set = " + set);
 
-
+        Double sum = (double) set.size();
+        answer = (int) ((cnt / sum) * 65336);
         return answer;
     }
 
@@ -34,12 +34,15 @@ public class 뉴스클러스터링 {
         for (int i = 0; i < str.length()-2+1; i++) {
             String st = "";
             for (int j = i; j < 2+i; j++) {
-                char ch = st.charAt(j);
+                char ch = str.charAt(j);
                 if(!Character.isLetter(ch)){
                     st = "";
                     break;
                 }
                 st += ch;
+            }
+            if (count == 1 && set.contains(st)){
+                cnt++;
             }
             if(!st.isEmpty()){
                 set.add(st);
